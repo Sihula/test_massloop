22ae0013ba470694209fcf2d7114427fe417c2f9
Sihula <serhiisihula@gmail.com>
2022-03-13 23:13:12 +0100

    end

--- app/js/main.js
+++ app/js/main.js
@@ -5,3 +5,8 @@ burger.addEventListener("click", () => {
   menu.classList.toggle("active");
   burger.classList.toggle("active");
 });
+
+let a = document.querySelector("#input-one");
+let b = document.querySelector("#input-two");
+
+document.querySelector(".summa").innerText = 12;
